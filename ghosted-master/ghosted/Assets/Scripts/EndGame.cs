8d11cd3974e167efcaf0a2a98b9a9afbfbffa4cf
DavidMMata <davidmmata89@gmail.com>
2022-06-07 05:36:01 -0600

    First commit

---
+++ ghosted-master/ghosted/Assets/Scripts/EndGame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    public bool isGameLost = false;
+    public bool isGameWon = false;
+    private float health = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+    void GameOver()
+    {
+        if (health == 0)
+        {
+            isGameLost = true;
+            showLossScreen();
+        }
+    }
+    
+    public void showLossScreen()
+    {
+
+    }
+    public void showWinScreen()
+    {
+
+    }
+}
